34fe807fe83fc48f9dd98289a63308eb31e25ac6
learn2expertize <55656847+learn2expertize@users.noreply.github.com>
2019-09-23 23:28:14 +0530

    Cache ServiceWorker

--- sw.js
+++ sw.js
@@ -1,6 +1,25 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 // install service worker
 self.addEventListener('install', evt => {
     //console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 
 // activate event
@@ -11,4 +30,9 @@ self.addEventListener('activate', evt => {
 // fetch event
 self.addEventListener('fetch', evt => {
     //console.log('service worker fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    )
 });
