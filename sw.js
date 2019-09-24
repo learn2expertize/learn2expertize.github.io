c18d38309a10334452c13fe9499ef9a1587c47fa
learn2expertize <55656847+learn2expertize@users.noreply.github.com>
2019-09-25 01:22:45 +0530

    Tuts 18 Completed

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v2';
+const dynamicCache = 'site-dynamic-v1'
 const assets = [
     '/',
     '/index.html',
@@ -25,6 +26,14 @@ self.addEventListener('install', evt => {
 // activate event
 self.addEventListener('activate', evt => {
     //console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 })
 
 // fetch event
@@ -32,7 +41,12 @@ self.addEventListener('fetch', evt => {
     //console.log('service worker fetch event', evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
         })
     )
 });
